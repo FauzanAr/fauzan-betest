ed2379e4232026bb636f2f35df6fa110708ef007
FauzanAr <faurinta12345@gmail.com>
2024-05-23 17:09:54 +0700

    feat(middleware): add auth middleware with jwt

---
+++ src/helpers/auth/jwt/middleware.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
+const wrapper = require('../../utils/wrapper');
+const { UnauthorizedError } = require('../../error');
+
+const verifyAuth = (req, res, next) => {
+    const key = config.auth.privateKey;
+    const auth = req?.headers?.authorization;
+    if (!auth) {
+        const response = wrapper.error(new UnauthorizedError('Authorization not found!'));
+        return wrapper.response(res, 'fail', response, 'Error Auth');
+    }
+
+    const token = auth.split(' ');
+    if (!token) {
+        const response = wrapper.error(new UnauthorizedError('Invalid Authorization token!'));
+        return wrapper.response(res, 'fail', response, 'Error Auth');
+    }
+
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, key);
+    } catch (error) {
+        console.log(error);
+        const response = wrapper.error(new UnauthorizedError('Failed to verify token!'));
+        return wrapper.response(res, 'fail', response, 'Error Auth');
+    }
+
+    req.user = decodedToken;
+    next();
+};
+
+const generateToken = (payload) => {
+    const expiresIn = 24 * 60 * 60;
+    const key = config.auth.privateKey;
+    const token = jwt.sign(payload, key, {expiresIn})
+    return token;
+};
+
+module.exports = {
+    verifyAuth,
+    generateToken,
+}
